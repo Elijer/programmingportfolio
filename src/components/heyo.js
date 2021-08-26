cdec9587e9b5a3dca7a1c659bdea79b4c585d493
Jah <kennedye2@vcu.edu>
2021-08-26 10:21:22 +0200

    scss is broken into modules as well

--- src/components/heyo.js
+++ src/components/heyo.js
@@ -3,4 +3,4 @@ import { createElement, createFragment } from '../utility/createElement'
 /** @jsx createElement */
 /*** @jsxFrag createFragment */
 
-export const heyo = <h1> HEYO </h1>
+export const heyo = <h1 id = "heyo"> HEYO </h1>
