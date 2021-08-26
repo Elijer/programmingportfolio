520d3e966c78ede62145807d5c1fdcf2ae8bc888
Jah <kennedye2@vcu.edu>
2021-08-26 10:15:22 +0200

    added heyo example component to component folder too

---
+++ src/components/heyo.js
@@ -0,0 +1,6 @@
+import { createElement, createFragment } from '../utility/createElement'
+
+/** @jsx createElement */
+/*** @jsxFrag createFragment */
+
+export const heyo = <h1> HEYO </h1>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { createElement, createFragment } from '/utility/createElement'
 import { UsingFragment } from '/components/usingFragment'
+import { heyo } from '/components/heyo'
 
 /** @jsx createElement */
 /*** @jsxFrag createFragment */
-
-  const anotherElement = <h1> HEYO </h1>
   
-  document.getElementById("root").appendChild(<UsingFragment name="foo" />);
-  document.getElementById("root").appendChild(anotherElement);
-  
+document.getElementById("root").appendChild(<UsingFragment name="foo" />);
+document.getElementById("root").appendChild(heyo);
