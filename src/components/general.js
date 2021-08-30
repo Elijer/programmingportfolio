e7482a6ee30f680209abf706628c095893b17d5d
Jah <kennedye2@vcu.edu>
2021-08-30 17:49:57 -0500

    grid comp styling specificity

--- src/components/general.js
+++ src/components/general.js
@@ -7,6 +7,7 @@ export const banner = <div id = "banner">
 <h1> ELIJAH KENNEDY </h1>
 </div>
 
-export const grid = <div id = "main-grid">
-    <h1> This is gonna be a grid </h1>
+export const grid =
+<div id = "main-grid">
+    <h1 id = "wtf" > This is gonna be a grid </h1>
 </div>
