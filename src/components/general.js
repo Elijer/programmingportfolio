8b8c449d2fe52cd8feb4c82ba5326e6e0fa65d60
Jah <kennedye2@vcu.edu>
2021-09-23 16:02:50 -0500

    using image, title, description data to render tiles

--- src/components/general.js
+++ src/components/general.js
@@ -10,49 +10,52 @@ export const banner =
     <h1> ELIJAH KENNEDY </h1>
 </div>
 
-var pieces = [
-    {
-        url: "https//i2.wp.com/www.knom.org/wp/wp-content/uploads/2019/09/Bull-Moose-stock-image-from-Pixabay.jpg?fit=1200%2C800&ssl=1",
-        title: "Moose",
-        id: "moose",
-        description: "This is a moose that I created with my godlike powers.",
-        stack: ["flesh", "blood", "grey-matter"],
-        link: "https://www.google.com/search?q=moose&oq=moose&aqs=chrome.0.35i39l2j46i131i433i512j69i60l5.446j0j1&sourceid=chrome&ie=UTF-8"
-    },
-
-]
-
 export const portfolio = () => {
+
+
+    var pieces = [
+        {
+            url: "https://i.natgeofe.com/n/21838d6e-005b-4ae9-a781-016bb156cf6c/01mooseWAQ.jpg",
+            title: "Moose",
+            id: "moose",
+            description: "This is a moose that I created with my godlike powers.",
+            stack: ["flesh", "blood", "grey-matter"],
+            link: "https://www.google.com/search?q=moose&oq=moose&aqs=chrome.0.35i39l2j46i131i433i512j69i60l5.446j0j1&sourceid=chrome&ie=UTF-8"
+        }
+
+    ]
+
     var port =
     <div id = "port" >
     </div>;
 
     append(port);
 
-    for (const piece of pieces ){
+    for (var piece of pieces){
+        console.log(piece.url);
     //for (var i = 0; i < 10; i++){
 
         const tile =
-        <div id = {"tile-" + i} class = "tile" >
+        <div id = {"tile-" + piece.id} class = "tile" >
 
             <img
-            id = {"tile-img" + i}
+            id = {"tile-img" + piece.id}
             class = "tile-img"
-            src = "https://i2.wp.com/www.knom.org/wp/wp-content/uploads/2019/09/Bull-Moose-stock-image-from-Pixabay.jpg?fit=1200%2C800&ssl=1" width = "300px">
+            src = {piece.url} >
             </img>
 
             <div class = "tile-body">
 
                 <h1
-                id = {"tile-title-"+{i}}
+                id = {"tile-title-"+piece.id}
                 class = "tile-title">
-                    Title {i}
+                    {piece.title}
                 </h1>
 
                 <p
-                id = {"tile-desc" + i}
+                id = {"tile-desc" + piece.id}
                 class = "tile-desc">
-                    Description {i*6}: This is a description for this title.
+                    {piece.description}
                 </p>
 
             </div>
