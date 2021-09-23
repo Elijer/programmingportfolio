7df8b6caed8ecd0c962bf51d1c6452c707dabd6e
Jah <kennedye2@vcu.edu>
2021-09-23 15:37:00 -0500

    some styling

--- src/components/general.js
+++ src/components/general.js
@@ -18,8 +18,33 @@ export const portfolio = () => {
     append(port);
 
     for (var i = 0; i < 10; i++){
-        const tile = <h1 id = {"tile-" + i} class = "tile" > Yohoo </h1>
+
+        const tile =
+        <div id = {"tile-" + i} class = "tile" >
+
+            <img
+            id = {"tile-img" + i}
+            class = "tile-img"
+            src = "https://i2.wp.com/www.knom.org/wp/wp-content/uploads/2019/09/Bull-Moose-stock-image-from-Pixabay.jpg?fit=1200%2C800&ssl=1" width = "300px">
+            </img>
+
+            <h1
+            id = {"tile-title-"+{i}}
+            class = "tile-title">
+                Title {i}
+            </h1>
+
+            <p
+            id = {"tile-desc" + i}
+            class = "tile-desc">
+                Description {i*6}: This is a description for this title.
+            </p>
+
+        </div>
+
+        // Finally, add the tile to the portfolio container
         document.getElementById("port").appendChild(tile);
+
     }
 
     return port;
