8c7db65f1dabd87a50803996894b9af64536a388
Jah <kennedye2@vcu.edu>
2021-09-23 15:11:38 -0500

    added a readme and deleted unused component in general.js

---
+++ README.md
@@ -0,0 +1,7 @@
+# Using JSX without React
+[This project heavily relies on this post](https://betterprogramming.pub/how-to-use-jsx-without-react-21d23346e5dc) to run.
+
+# My JS in HTML paradigm
+The problem is that if you are appending the SAME element over and over again within a loop,
+Nothing happens, because the same element is being appended over again!
+I guess? Seems weird. I don't totally understand, but I'm glad it works.
--- src/components/general.js
+++ src/components/general.js
@@ -5,11 +5,10 @@ import { el, mount } from "redom";
 /** @jsx createElement */
 /*** @jsxFrag createFragment */
 
-export const banner = <div id = "banner">
-<h1> ELIJAH KENNEDY </h1>
+export const banner =
+<div id = "banner">
+    <h1> ELIJAH KENNEDY </h1>
 </div>
-
-export const someEl = <h1> Yohoo</h1>
 
 export const test = () => {
     var container =
@@ -18,9 +17,6 @@ export const test = () => {
 
     append(container);
 
-    // The problem is that if you are appending the SAME element over and over again within a loop,
-    // Nothing happens, because the same element is being appended over again!
-    // I guess? Seems weird. I don't totally understand, but I'm glad it works.
     for (var i = 0; i < 10; i++){
         const someEl = <h1> Yohoo </h1>
         document.getElementById("target").appendChild(someEl);
