ff6a82d3271f56e18576f20fba6b767672ca001a
Jah <kennedye2@vcu.edu>
2021-09-25 20:23:32 -0500

    cleaned up the includes a bit

---
+++ src/components/banner.js
@@ -0,0 +1,10 @@
+/** @jsx createElement */
+/*** @jsxFrag createFragment */
+
+export const banner =
+<div id = "banner">
+    <h1>ELIJAH KENNEDY
+{/* <span class = "first-name">ELIJAH </span>
+<span class = "last-name">KENNEDY</span> */}
+    </h1>
+</div>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import { createElement, createFragment, append } from '/utility/jsx'
-import { UsingFragment } from '/components/usingFragment'
 import { banner, portfolio, someEl } from '/components/general'
 
 /** @jsx createElement */
 /*** @jsxFrag createFragment */
   
-// document.getElementById("root").appendChild(banner);
-//document.getElementById("root").appendChild(<UsingFragment name="foo" />);
 append(banner);
 append(portfolio());
