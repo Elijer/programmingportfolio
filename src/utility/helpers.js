43e2ae029c1610ed72cf341b970fb2e0b691270d
Jah <kennedye2@vcu.edu>
2021-09-29 09:28:27 -0500

    tweaks to scrolling video play behavior for mobile

--- src/utility/helpers.js
+++ src/utility/helpers.js
@@ -4,11 +4,12 @@ export function gg(id){
 
 export function isInViewport(element) {
     const rect = element.getBoundingClientRect();
-    const buffer = 100;
+    const bufferTop = 100;
+    const bufferBottom = 70;
     return (
-        rect.top >= 0 + buffer &&
+        rect.top >= 0 + bufferTop &&
         //rect.left >= 0 &&
-        rect.bottom <= ((window.innerHeight || document.documentElement.clientHeight)-buffer)
+        rect.bottom <= ((window.innerHeight || document.documentElement.clientHeight)-bufferBottom)
         //&& rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
